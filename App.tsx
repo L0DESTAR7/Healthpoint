0439a9b6025c9104476f608ac63a2d07ed2dd486
Lodestar7 <godhand2006@hotmail.fr>
2023-12-14 04:48:00 +0100

    feat: 🎸 Added a few dependencies & Folder structure

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from './src/tailwind';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={tw`text-spring-500`}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#00FFA3'
+  }
 });
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'spring': {
+          '50': '#edfff8',
+          '100': '#d5fff0',
+          '200': '#aeffe2',
+          '300': '#70ffcd',
+          '400': '#2bfdb1',
+          '500': '#00ffa3',
+          '600': '#00c076',
+          '700': '#009660',
+          '800': '#06754e',
+          '900': '#076042',
+          '950': '#003724',
+        },
+      },
+      fontSize: {
+        '2xs': '0.625rem',
+        '3xs': '0.5rem',
+        '4xs': '0.375rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
