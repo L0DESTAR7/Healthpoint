d0b08479db74b1e4b008abae601f5cb0117fb64e
Lodestar7 <godhand2006@hotmail.fr>
2023-12-17 06:51:38 +0100

    refactor: 💡 Added User type and fixture

--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -1,15 +1,16 @@
 import { Text } from "react-native";
+import DefaultUser from "../fixtures/DefaultUser";
 
 
 
 export default function HeaderTitle() {
 
-  // TODO:: Implement getUsername function and use it.
+  const username = DefaultUser.name;
 
   return (
     <Text className="font-lexend-semibold text-spring-500 text-3xl md:text-4xl lg:text-6xl">
       <Text className="font-lexend-medium text-spring-950 dark:text-white">
-        Hello, </Text>{"Karim."}
+        Hello, </Text>{username + "."}
     </Text>
   );
 }
---
+++ src/fixtures/DefaultUser.ts
@@ -0,0 +1,10 @@
+import { User } from "../types/User"
+
+
+const DefaultUser: User = {
+  name: "Karim",
+  age: 30,
+  diabetesType: "A",
+}
+
+export default DefaultUser;
