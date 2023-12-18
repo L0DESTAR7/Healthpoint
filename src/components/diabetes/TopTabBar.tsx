9fc73214eae94241cee311f4de7dc3b2c8fd3e62
Lodestar7 <godhand2006@hotmail.fr>
2023-12-18 06:37:38 +0100

    fix: 🐛 Fixed Section Navigation bug in Diabetes section
    Fixed a bug where taping a section you're already on triggers a section
    change.

--- src/components/diabetes/TopTabBar.tsx
+++ src/components/diabetes/TopTabBar.tsx
@@ -8,19 +8,26 @@ export default function TopTabBar(props: MaterialTopTabBarProps) {
   console.log("[TopTabBar.tsx]: " + props.state.index);
   const index = props.state.index;
 
-  const handleOnPress = () => {
+  const handleOnPressOverview = () => {
+    index === 1 ?
+      props.navigation.navigate("Overview")
+      :
+      {}
+  }
+
+  const handleOnPressSimulation = () => {
     index === 0 ?
       props.navigation.navigate("Simulation")
       :
-      props.navigation.navigate("Overview")
+      {}
   }
 
   return (
     <View className="flex flex-row my-6 md:my-7 lg:my-12 items-center justify-between w-full h-10 md:h-12 lg:h-20 dark:bg-Darkbg rounded-full">
-      <TouchableOpacity activeOpacity={index === 0 ? 1 : 0.3} onPress={handleOnPress}>
+      <TouchableOpacity activeOpacity={index === 0 ? 1 : 0.3} onPress={handleOnPressOverview}>
         <SectionChip isActive={index === 0} label="Overview"></SectionChip>
       </TouchableOpacity>
-      <TouchableOpacity activeOpacity={index === 1 ? 1 : 0.3} onPress={handleOnPress}>
+      <TouchableOpacity activeOpacity={index === 1 ? 1 : 0.3} onPress={handleOnPressSimulation}>
         <SectionChip isActive={index === 1} label="Simulation"></SectionChip>
       </TouchableOpacity>
     </View>
